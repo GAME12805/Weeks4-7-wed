c00d172e40f5b26f21fc60f1219a61e854c5050b
DESKTOP-111JM1H\Kit Barry <kit.barry@sheridancollege.ca>
2026-01-28 13:52:47 -0500

    Completed coding gym

--- Assets/Scripts/UIDemo.cs
+++ Assets/Scripts/UIDemo.cs
@@ -7,7 +7,7 @@ public class UIDemo : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
+        sr = GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
